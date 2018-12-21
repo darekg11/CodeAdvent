2e0c72aa0acd8c2f5a9b7a3b9114eea7988fd224
Dariusz Górak <darekg11@tlen.pl>
2018-12-21 23:04:39 +0100

    2018 - Day19 - part 2

--- 2018/Day19/main.py
+++ 2018/Day19/main.py
@@ -151,4 +151,48 @@ def main():
 
     # Part 1 - print reg 0
     print(registers[0])
+
+    # Part 2 - copy paste but start with value 1
+    # This will go on for a really logn period of time so we need something better than bruteforcing
+    program_counter = 0
+    registers = [1, 0, 0, 0, 0, 0]
+    looper = 0
+    # Program loop
+    # UNCOMMENT WHILE BLOCK TO UNDERSTAND WHAT IS GOING ON
+    # while program_counter >= 0 and program_counter < len(instructions):
+    #     # if registers[instruction_pointer_register] == 1:
+    #     #     print (sum([x for x in range(1, registers[5]+1) if registers[5] % x == 0]))
+    #     #     break
+    #     # get instruction to execute
+    #     instruction_to_execute = instructions[registers[instruction_pointer_register]]
+    #     # Let's check what is in registers and instruction that is suppose to execute
+    #     if looper % 400_000 == 0:
+    #         # Stop it manually when you notice the pattern
+    #         print(instruction_to_execute, registers)
+    #     # there is a loop going on from instruction 3 - 11
+    #     op_code = instruction_to_execute[0]
+    #     param_a = instruction_to_execute[1]
+    #     param_b = instruction_to_execute[2]
+    #     output = instruction_to_execute[3]
+
+    #     # Execute
+    #     registers = call_map[op_code](registers, param_a, param_b, output)
+    #     # update PC
+    #     registers[instruction_pointer_register] += 1
+    #     program_counter = registers[instruction_pointer_register]
+    #     looper += 1
+
+    # What Part 2 is actually doing:
+    # instruction 3 - 11 is actually a loop
+    # r[5] stores some really big number, for me it was 10551296
+    # r[2] goes is outter loop like for i = 1; i <= r5 which is 10551296 for me
+    # r[3] is an inner loop going for j = 1; j <= r5 which is 10551296 for me
+    # inside the inner loop this is going on: if r[2] * r[3] == r[5] then r[0] += r[2]
+    # so what it is really doing is trying to find sum of every number that divides r[5] with 0 leftover aka modulo aka r[5] % x == 0
+
+    # Actual Part 2
+    # for explanation, read above!
+    my_input = 10551296
+    sum_of_every_number = sum([i for i in range(1, my_input + 1) if my_input % i == 0])
+    print(sum_of_every_number)
 main()
